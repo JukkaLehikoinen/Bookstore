69e3b2374c39a3f7310de3ffc34c3ec515b22f71
Jukka Lehikoinen <lehikoinen80@gmail.com>
2020-02-07 16:14:52 +0200

    C3.1 tehtävä suoritettu

---
+++ src/main/java/hh/hommia/Bookstore/domain/BookRepository.java
@@ -0,0 +1,12 @@
+package hh.hommia.Bookstore.domain;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book, Long>{
+
+	List<Book> findByIsbn(String isbn);
+	
+}
+
--- src/main/java/hh/hommia/Bookstore/webcontroller/BookController.java
+++ src/main/java/hh/hommia/Bookstore/webcontroller/BookController.java
@@ -2,16 +2,22 @@ package hh.hommia.Bookstore.webcontroller;
 
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import hh.hommia.Bookstore.domain.BookRepository;
+
 @Controller
 public class BookController {
-
+	@Autowired
+	private BookRepository repository; 
 	
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
-	public String getBooks() {
+	public String getBooks(Model model) {	
+        model.addAttribute("books", repository.findAll());
 	
 		return "index";
 	}
