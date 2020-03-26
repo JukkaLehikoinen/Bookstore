acca40e5c3eae38f72ef79fc77f06d510fc78d10
Jukka Lehikoinen <lehikoinen80@gmail.com>
2020-03-26 15:01:27 +0200

    smoketest

--- src/test/java/hh/hommia/Bookstore/BookstoreApplicationTests.java
+++ src/test/java/hh/hommia/Bookstore/BookstoreApplicationTests.java
@@ -1,15 +1,24 @@
 package hh.hommia.Bookstore;
 
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+
+import hh.hommia.Bookstore.webcontroller.BookController;
 
 
 @SpringBootTest
 class BookstoreApplicationTests {
+	 @Autowired
+	    private BookController controller;
 
-	@Test
-	void contextLoads() {
+	    @Test
+	    public void contexLoads() throws Exception {
+	        assertThat(controller).isNotNull();
+	        assertThat(controller.bookRest().get(0).getTitle().equals("Lord of the rings"));
+	        
+	    }	
 	}
-
-}
